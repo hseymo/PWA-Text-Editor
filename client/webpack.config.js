95c356c3966762f6936ea73612542671fe361bec
Haley Seymour <haleycseymour@comcast.net>
2022-05-24 11:55:59 -0700

    package.json scripts, webpack config workbox plugins for service worker and manifest file; add css loader and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,67 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      // cards: './src/js/cards.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack pligin to generate html and inject bundle
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        // add a better title
+        title: 'Notes or Code Snippets'
+      }),
+
+      // Inject custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Create manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Notes or Code Snippets',
+        short_name: 'Notes',
+        description: 'Nodes or Code Snippets',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loader
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
